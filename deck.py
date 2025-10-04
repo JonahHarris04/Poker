48beae1fa427343ec306c179bb57f922f25c89e4
Owen Davis <143449687+0Davos@users.noreply.github.com>
2025-10-04 11:51:19 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,5 +1,20 @@
+import random
+
 class Deck:
     def __init__(self):
         suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
         ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         self.cards = [Card(rank, suit) for suit in suits for rank in ranks]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+        
+    def take_card_from_top():
+        if self.cards:
+            return self.cards.pop(0)
+        else:
+            return None
+            
+    def __len__(self):
+        return len(self.cards)
+        
