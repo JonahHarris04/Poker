d6c2a57bc3e71e94f9f0e352d1c0708527867e83
Owen Davis <143449687+0Davos@users.noreply.github.com>
2025-10-04 12:04:35 -0400

    Initialize game.py

---
+++ game.py
@@ -0,0 +1,19 @@
+# Adding comments for no limit texas hold-em poker rules
+
+
+# 0) game, dealer, deck, pot, and each player is created
+
+# 1) Each player puts in a small bet 
+# (or we could do big blind, small blind, but that takes more effort. Would include creating a moving "dealer button", which the big and small blind follow)
+
+# 2) Two cards are then given to each person, rotating clockwise order (give each player one, then each player their second)
+#     Cards dealt starting with person to player left of button
+
+# 3) Initial betting occurs, person to left of small and big blind starts. Minimum raise is double the big blind.
+
+
+# Fold, Call, Raise
+
+# game needs to have at least two non-folded hands to continue playing
+# call and raise only possible if a player has the funds to do so
+
