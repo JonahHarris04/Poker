6433383c50abdf57c68f5bda3f6cc34373814f6d
Jake Pappas <jgpappas@uvm.edu>
2025-10-29 13:48:58 -0400

    Ace is now high card for [high_card, one_pair, two_pair, three_of_a_kind, flush, four_of_a_kind]. Straight: Ace can be high if 10-A. Fullhouse builds on three_of_a_kind/two_pair so the logic follows

--- rankings.py
+++ rankings.py
@@ -6,7 +6,7 @@ rank_number_to_string = {1: "A", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7",
 hand_rank_number_to_string = {1: "High Card", 2: "One Pair", 3: "Two Pair", 4: "Three of a Kind", 5: "Straight",
                               6: "Flush", 7: "Full House", 8: "Four of a Kind", 9: "Straight Flush", 10: "Royal Flush"}
 
-
+# Note: Ace card_number is 14 if it is used as a high card
 def rank_hand(full_hand):
     # if has_royal_flush(full_hand):
     #     return 10, None
@@ -48,11 +48,13 @@ def has_straight_flush(full_hand):
 
 
 # return whether there's a four of a kind and what the card_number is
-# AAAA -> True, A
+# AAAA -> True, 14
 def has_four_of_a_kind(full_hand):
     full_hand_numbers = [card.number for card in full_hand]
     for card_number in full_hand_numbers:
         if full_hand_numbers.count(card_number) == 4:
+            if card_number == 1:
+                card_number = 14
             return True, card_number
     return False, 0
 
@@ -69,7 +71,6 @@ def has_full_house(full_hand):
     return False, (0, 0)
 
 
-# TODO: ace high flush
 # returns whether there's a flush and what the high card of the flush is
 # 2H 5H 6H 10H 7H -> True, 10
 def has_flush(full_hand):
@@ -83,17 +84,19 @@ def has_flush(full_hand):
             flush_suit = suit
     if has_flush_bool:
         full_hand_numbers.sort(reverse=True)
+        for card in full_hand:
+            if card.suit == flush_suit and card.number == 1:
+                return True, 14
         for number in full_hand_numbers:
-            print(rank_number_to_string[number])
             for card in full_hand:
                 if card.suit == flush_suit and card.number == number:
                     return True, number
     return False, 0
 
 
-# TODO: high ace straight
 # return whether there's a straight and what the highest card of the straight is.
 # A2345 -> True, 5
+# 10JQKA -> True, A
 def has_straight(full_hand):
     has_straight_bool = False
     # a straight requires 5 cards minimum
@@ -111,6 +114,12 @@ def has_straight(full_hand):
 def check_straight_offsets(card, full_hand):
     capture = 0
     for i in range(1, 5):
+        # special case for Ace high
+        if card.number + i == 14:
+            if not value_in_full_hand(1, full_hand):
+                return 0
+            return 14
+
         if not value_in_full_hand(card.number + i, full_hand):
             return 0
         capture = i
@@ -124,16 +133,18 @@ def value_in_full_hand(card_number, full_hand):  # probably make static I guess
     return False
 
 
-# TODO: ace highest
 # returns whether there's a three of a kind and the best three of a kind card
 # 3335557 -> True, 5
 def has_three_of_a_kind(full_hand):
     full_hand_numbers = [card.number for card in full_hand]
     current_best = 0
     for card_number in full_hand_numbers:
-        if card_number > current_best:
+        if card_number > current_best or card_number == 1:
             if full_hand_numbers.count(card_number) == 3:
                 current_best = card_number
+                if current_best == 1:
+                    return True, 14
+
     return current_best > 0, current_best
 
 
@@ -143,8 +154,11 @@ def has_two_pair(full_hand):
     full_hand_numbers = [card.number for card in full_hand]
     pairs = []
     for card_number in full_hand_numbers:
-        if full_hand_numbers.count(card_number) >= 2 and card_number not in pairs:
-            pairs.append(card_number)
+        if full_hand_numbers.count(card_number) >= 2:
+            if card_number == 1:
+                card_number = 14
+            if card_number not in pairs:
+                pairs.append(card_number)
     if len(pairs) > 2:
         pairs.remove(min(pairs))
     return len(pairs) == 2, pairs
@@ -156,6 +170,8 @@ def has_one_pair(full_hand):
     full_hand_numbers = [card.number for card in full_hand]
     for card_number in full_hand_numbers:
         if full_hand_numbers.count(card_number) == 2:
+            if card_number == 1:
+                return True, 14
             return True, card_number
     return False, 0
 
@@ -164,12 +180,12 @@ def has_one_pair(full_hand):
 # 28974 -> True, 9
 def has_high_card(full_hand):
     full_hand_numbers = [card.number for card in full_hand]
-    if 1 in full_hand_numbers:  # Ace: maybe should emit 14? we'll see
-        return True, 1
+    if 1 in full_hand_numbers:  # Ace
+        return True, 14
     return True, max(full_hand_numbers)
 
 
 ranking = rank_hand(
-    [Card.Card("Hearts", "A"), Card.Card("Spades", "3"), Card.Card("Clubs", "5"), Card.Card("Hearts", "4"),
-     Card.Card("Hearts", "2"), Card.Card("Spades", "2"), Card.Card("Diamonds", "6")])
+    [Card.Card("Hearts", "K"), Card.Card("Hearts", "2"), Card.Card("Clubs", "3"), Card.Card("Hearts", "5"),
+     Card.Card("Hearts", "A"), Card.Card("Hearts", "3"), Card.Card("Diamonds", "3")])
 print(hand_rank_number_to_string[ranking[0]], ranking[1])
